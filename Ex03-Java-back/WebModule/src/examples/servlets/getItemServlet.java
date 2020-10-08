39d92dd16f620230b74d1a5a7625a84d19674249
Dor Cohen <doric93@gmail.com>
2020-10-08 12:59:33 +0300

    fetching data servlet

--- Ex03-Java-back/WebModule/src/examples/servlets/getItemServlet.java
+++ Ex03-Java-back/WebModule/src/examples/servlets/getItemServlet.java
@@ -1,6 +1,9 @@
 package examples.servlets;
 
-import SDMModel.*;
+import SDMModel.Item;
+import SDMModel.Store;
+import SDMModel.SuperMarket;
+import SDMModel.SystemManager;
 import com.google.gson.Gson;
 import com.google.gson.JsonObject;
 
@@ -12,47 +15,33 @@ import javax.servlet.http.HttpServletResponse;
 import javax.servlet.http.HttpSession;
 import java.io.IOException;
 import java.util.HashMap;
-import java.util.List;
 import java.util.stream.Collectors;
 
 
-@WebServlet(name = "getDataServlet", urlPatterns = "/getData")
-public class getDataServlet extends HttpServlet {
+@WebServlet(name = "getItemsServlet", urlPatterns = "/getItems")
+public class getItemServlet extends HttpServlet {
 
     public void processRequest(HttpServletRequest request, HttpServletResponse response)
             throws ServletException, IOException {
+
         Gson gson = new Gson();
         HttpSession session = request.getSession();
         String rawRequestData = request.getReader().lines().collect(Collectors.joining());
         JsonObject requestData = new Gson().fromJson(rawRequestData, JsonObject.class);
 
-
         response.setContentType("text/html;charset=UTF-8");
         response.setHeader("Access-Control-Allow-Origin", "*");
-
         if(requestData.has("zone")){
             SuperMarket superMarket = SystemManager.getInstance().getSuperMarketByLocation(requestData.get("zone").getAsString());
-            if(!requestData.has("storeId") || requestData.get("storeId").getAsString().equals(""))
-                response.getWriter().append(gson.toJson(superMarket));
-            else{
-                Store store = superMarket.getStores().get(requestData.get("storeId").getAsInt());
-                if((!requestData.has("itemId") || requestData.get("itemId").getAsString().equals("") &&
-                        !requestData.has("sales") || requestData.get("sales").getAsString().equals("")))
-                    response.getWriter().append(gson.toJson(store));
-                else{
-                    if(requestData.has("itemId") && !requestData.get("itemId").getAsString().equals("")) {
-                        Sell sell = store.getSellById(requestData.get("itemId").getAsInt());
-                        response.getWriter().append(gson.toJson(sell));
-                    }
-                    else {
-                        response.getWriter().append(gson.toJson(store.getSales()));
-                    }
-                }
+            if(requestData.has("item") && !requestData.get("item").equals("")){
+                Item item = superMarket.getItemByID(requestData.get("item").getAsInt());
+                response.getWriter().append(gson.toJson(item));
             }
+            else
+                response.getWriter().append(gson.toJson(superMarket.getItems()));
         }
 
     }
-
 
     @Override
     protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
