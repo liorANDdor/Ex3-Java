e05791dbbaf6351b0a30f97c78fa42ebee4b1bc2
Dor Cohen <doric93@gmail.com>
2020-10-26 20:26:55 +0200

    feedbacks with starts

--- Ex03-Java-front/sdmfront/src/Components/LogicalComponents/showFeedbacks/ShowFeedbacks.js
+++ Ex03-Java-front/sdmfront/src/Components/LogicalComponents/showFeedbacks/ShowFeedbacks.js
@@ -4,12 +4,13 @@ import { withRouter } from "react-router-dom";
 import { makeStyles } from '@material-ui/core/styles';
 import axios from "../../../Utilities/Axios/Axios";
 import Table from "../../UIComponents/Table/Table";
+import Rating from '@material-ui/lab/Rating';
 
 
 const useStyles = makeStyles((theme) => ({
     container: {
         background: 'inherit',
-        width: '30%',
+        width: '80%',
         marginLeft: '20%',
         marginTop: '2%',
         height: '200px',
@@ -24,6 +25,9 @@ const ShowFeedbacks = (props) => {
     let cols = {
         Rate : {
             header: 'Rate',
+        },
+        Store:{
+            header: 'Store',
         },
         Message : {
             header: 'Message',
@@ -59,8 +63,8 @@ const ShowFeedbacks = (props) => {
         <div>
             <Table
                 columns={cols}
-                    data={feedbacks.map(feedback => ({  Client:feedback.rating, Date: feedback.comment ,
-                Rate: feedback.feedbackDate , Message:feedback.clientName}))}
+                    data={feedbacks.map(feedback => ({  Client: <Rating name="read-only" value={feedback.rating} readOnly />, Store: feedback.storeName,
+                        Date: feedback.comment , Rate: feedback.feedbackDate , Message:feedback.clientName}))}
                 container={classes.container} /> </div>
 )
 };
