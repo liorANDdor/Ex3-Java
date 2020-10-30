8bc441257c284eccd0cc0b066b6268b7b4f3a616
Dor Cohen <doric93@gmail.com>
2020-10-30 11:32:51 +0200

    orders

--- Ex03-Java-back/WebModule/src/examples/servlets/getOrdersServlet.java
+++ Ex03-Java-back/WebModule/src/examples/servlets/getOrdersServlet.java
@@ -40,13 +40,54 @@ public class getOrdersServlet extends HttpServlet {
     public void processRequest(HttpServletRequest request, HttpServletResponse response)
             throws ServletException, IOException {
         Gson gson = new Gson();
+        HashMap<Integer, JSONObject> ordersInfo = new HashMap<>();
         String userName = (String) request.getSession().getAttribute("userName");
         HashMap<Integer, Order> orders =  SystemManager.getInstance().getUsers().get(userName).getOrders();
+        for (Order order:orders.values()){
+            ordersInfo.put(order.getOrderNumber(), makeOrdersObject(order));
+        }
         response.setContentType("text/html;charset=UTF-8");
         response.setHeader("Access-Control-Allow-Origin", "*");
         response.getWriter().append(gson.toJson(orders));
 
     }
+
+    private JSONObject makeOrdersObject(Order order) {
+            JSONObject orderInfo = new JSONObject();
+            orderInfo.put("id", order.getOrderNumber());
+            orderInfo.put("date", order.getDateOfOrder());
+            orderInfo.put("storesCount", order.getStoresToOrderFrom().size());
+            orderInfo.put("customerLocation", order.getLocationOfClient());
+            orderInfo.put("itemsPrice", order.getItemsPrice());
+            orderInfo.put("shipmentPrice", order.getShipmentPrice());
+            orderInfo.put("itemsAmound", order.getItemsQuantity().size());
+            orderInfo.put("items", createItemsObject(order));
+
+            orderInfo.put(order.getOrderNumber().toString(), orderInfo);
+
+        return orderInfo;
+
+    }
+
+    private JSONObject createItemsObject(Order order) {
+        JSONObject itemsInfo = new JSONObject();
+        for(Item item:order.getItemsQuantity().keySet()){
+            JSONObject itemInfo = new JSONObject();
+
+            itemInfo.put("id", item.getId());
+            itemInfo.put("name", item.getName());
+
+            itemInfo.put("purchaseType", item.getPurchaseCategory());
+            itemInfo.put("quantity", order.getItemsQuantity().get(item));
+            itemInfo.put("price", order.getItemPrice(item.getId()));
+            itemInfo.put("totalPrice", order.getItemPrice(item.getId()) * order.getItemsQuantity().get(item) );
+            itemInfo.put("isFromDiscount", "No  ");
+            itemsInfo.put(String.valueOf(item.getId()), itemInfo);
+
+        }
+        return itemsInfo;
+    }
+
 
     @Override
     protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
